63a11b60fce00ca18e04a077bbc0408a6ddbebfb
Zen64 <xxzenshadowxx@gmail.com>
2025-11-14 02:46:21 -0500

    Formatting, configuration, movement types
    Formatted project with Black
    Implemented enum configurations for different forms of collision
    Configurations are json serializable to allow for easy custom pets

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-#region To-do
-    # Safer file handling (load once, then close)
-        # Should be good but re-check everything
-    # Safer error handling
-        # Should be good but re-check everything
-    # Fix 'NoneType' object has no attribute 'stop' on launch
-    # Implement enum options for creating customized pets
-#endregion
+# region To-do
+# Safer file handling (load once, then close)
+# Should be good but re-check everything
+# Safer error handling
+# Should be good but re-check everything
+# DONE Fix 'NoneType' object has no attribute 'stop' on launch
+# DONE Implement enum options for creating customized pets
+# Add setup.py
+# DONE Fix tray Quit not working
+# Physics ("grounded" pets)
+# endregion
 
-#region Imports
+# region Imports
 from PIL import Image
 from screeninfo import get_monitors
 import ctypes
@@ -17,32 +20,51 @@ import tkinter as tk
 
 import assets as py_assets
 import pet as py_pet
-#endregion
 
-#region Global variables
+# endregion
+
+# region Global variables
 global app_ctx
 
-class AppContext():
-    def __init__(self, root, canvas, pets, displays, tray_icon):
+
+class AppContext:
+    def __init__(self, root, canvas, pets: py_pet.Pet, displays, tray_icon):
         self.root = root
         self.canvas = canvas
         self.pets = pets
         self.displays = displays
         self.tray_icon = tray_icon
-#endregion
 
-#region Tray icon methods
+
+# endregion
+
+
+# region Tray icon methods
 def setup_tray():
     global app_ctx
-    
+
     try:
         icon_image = Image.open("assets\\images\\icon.png")
     except Exception as e:
         print(e)
 
+    default_pet_config = py_pet.PetConfig(
+        name="default",
+        spawn_amount=1,
+        move_speed=25,
+        initial_position=[0, 0],
+        initial_position_type=py_pet.InitialPositionType.RANDOMIZED,
+        border_reaction_type=py_pet.BorderReactionType.BOUNCE,
+        direction_type=py_pet.DirectionType.RANDOMIZED_ONCE,
+        image_path="assets\\images\\cat.png",
+        dialogue=[],
+    )
+
     menu = pystray.Menu(
-        pystray.MenuItem("Spawn", py_pet.spawn_pet(image_path="assets\\images\\cat.png", ctx=app_ctx)),
-        pystray.MenuItem("Quit", quit_tray(ctx=app_ctx)),
+        pystray.MenuItem(
+            "Spawn", py_pet.spawn_pet(app_ctx, pet_config=default_pet_config)
+        ),
+        pystray.MenuItem("Quit", quit_tray),
     )
 
     tray_icon = pystray.Icon(
@@ -51,39 +73,37 @@ def setup_tray():
         title="deskpet",
         menu=menu,
     )
-    
+
     app_ctx.tray_icon = tray_icon
 
-def quit_tray(ctx):
+
+def quit_tray():
+    global app_ctx
+
     try:
-        ctx.tray_icon.stop()
+        app_ctx.root.quit()
     except Exception as e:
         print(e)
 
-    try:
-        if ctx.root is not None:
-            ctx.root.quit()
-    except Exception as e:
-        print(e)
-#endregion
 
-#region Window setup methods
+# endregion
+
+
+# region Window setup methods
 def setup_canvas():
     global app_ctx
 
     app_ctx = AppContext(
-        root=tk.Tk(),
-        canvas=None,
-        pets=[],
-        displays=get_monitors(),
-        tray_icon=None
+        root=tk.Tk(), canvas=None, pets=[], displays=get_monitors(), tray_icon=None
     )
-    
+
     # Setup transparent window
     app_ctx.root.overrideredirect(True)
-    app_ctx.root.wm_attributes('-transparentcolor', 'white') # make white pixels transparent
-    app_ctx.root.config(bg='white') # background color
-    app_ctx.root.wm_attributes('-topmost', True) # always on top of all windows
+    app_ctx.root.wm_attributes(
+        "-transparentcolor", "white"
+    )  # make white pixels transparent
+    app_ctx.root.config(bg="white")  # background color
+    app_ctx.root.wm_attributes("-topmost", True)  # always on top of all windows
 
     # Get window handle
     hwnd = ctypes.windll.user32.GetParent(app_ctx.root.winfo_id())
@@ -91,20 +111,45 @@ def setup_canvas():
     # Set layered and transparent styles
     extended_style = ctypes.windll.user32.GetWindowLongW(hwnd, -20)
     ctypes.windll.user32.SetWindowLongW(hwnd, -20, extended_style | 0x80000 | 0x20)
-    
-    #region Setup canvas
-    app_ctx.canvas = tk.Canvas(app_ctx.root, bg="white",
-        width=app_ctx.displays[0].width, height=app_ctx.displays[0].height, highlightthickness=0)
-    
+
+    # region Setup canvas
+    app_ctx.canvas = tk.Canvas(
+        app_ctx.root,
+        bg="white",
+        width=app_ctx.displays[0].width,
+        height=app_ctx.displays[0].height,
+        highlightthickness=0,
+    )
+
     app_ctx.canvas.pack(fill="both")
     app_ctx.root.after(0, update_loop)
-    #endregion
-#endregion
+    # endregion
 
-#region Main loop
+
+# endregion
+
+
+# region Main loop
+def spawn_test_pets():
+    config_options = py_pet.PetConfig(
+        name="test pet",
+        spawn_amount=32,
+        initial_position=[128, 128],
+        move_speed=0,
+        initial_position_type=py_pet.InitialPositionType.RANDOMIZED,
+        border_reaction_type=py_pet.BorderReactionType.BOUNCE,
+        direction_type=py_pet.DirectionType.RANDOMIZED_ONCE,
+        image_path="assets\\images\\cat.png",
+        dialogue=None,
+    )
+
+    for i in range(0, config_options.spawn_amount):
+        py_pet.spawn_pet(app_ctx, pet_config=config_options)
+
+
 def main():
     global app_ctx
-    
+
     setup_canvas()
     setup_tray()
 
@@ -112,19 +157,19 @@ def main():
     if app_ctx.tray_icon is not None:
         threading.Thread(target=app_ctx.tray_icon.run, daemon=True).start()
 
-    for i in range(0,8):
-        py_pet.spawn_pet(app_ctx, image_path="assets\\images\\cat.png")
+    spawn_test_pets()
 
     py_assets.pet_to_json(app_ctx.pets[0])
 
     app_ctx.root.mainloop()
 
+
 def update_loop():
     global app_ctx
-    
+
     py_pet.update_pets(app_ctx)
-    #p_root.after(20, py_pet.py_update_pets)
-    
+
+
 if __name__ == "__main__":
     main()
-#endregion
+# endregion
--- pet.py
+++ pet.py
@@ -1,104 +1,187 @@
-#region Imports
+# region Imports
 from enum import Enum
 import assets as py_assets
 import random
 import tkinter as tk
-#endregion
 
-#region Pet class and methods
+# endregion
+
+
+# region Configuration
+class InitialPositionType(Enum):
+    RANDOMIZED = "RANDOMIZED"
+    SET = "SET"
+
+
+class BorderReactionType(Enum):
+    BOUNCE = "BOUNCE"
+    WRAP = "WRAP"
+
+
+class DirectionType(Enum):
+    RANDOMIZED_ONCE = "RANDOMIZED_ONCE"
+
+
+class PetConfig:
+    def __init__(
+        self,
+        name,
+        spawn_amount,
+        initial_position,
+        move_speed,
+        initial_position_type: InitialPositionType,
+        border_reaction_type: BorderReactionType,
+        direction_type: DirectionType,
+        image_path,
+        dialogue,
+    ):
+        self.name = name
+        self.spawn_amount = spawn_amount
+        self.initial_position = initial_position
+        self.move_speed = move_speed
+        self.initial_position_type = (initial_position_type,)
+        self.border_reaction_type = (border_reaction_type,)
+        self.direction_type = (direction_type,)
+        self.image_path = image_path
+        self.dialogue = dialogue
+
+
+# endregion
+
+
+# region Pet class and methods
 class Pet:
     """
     position [x,y]
-    current_direction [x,y]
+    velocity [x,y]
     """
-    def __init__(self, name, position, move_speed, current_direction, shape, tk_img):
+
+    def __init__(
+        self, name, position, move_speed, velocity, shape, tk_img, pet_config: PetConfig
+    ):
         self.name = name
         self.position = position.copy()
         self.move_speed = move_speed
-        self.current_direction = current_direction
+        self.velocity = velocity
         self.shape = shape
         self.tk_img = tk_img
+        self.pet_config = pet_config
 
     def speak(self):
         return f"i am {self.name}"
 
     def get_pos(self):
         return f"{self.name} @ {self.position}"
-    
+
     def move(self, ctx, dx, dy):
         ctx.canvas.move(self.shape, dx, dy)
         self.position[0] += dx
         self.position[1] += dy
-    
+
     def set_pos(self, ctx, x, y):
         ctx.canvas.moveto(self.shape, x, y)
         self.position[0] = x
         self.position[1] = y
-    
+
     def drag_pet_start(self, event):
         self.drag_start_x = event.x
         self.drag_start_y = event.y
-    
+
     def drag_pet_move(self, event, ctx):
         dx = event.x - self.drag_start_x
         dy = event.y - self.drag_start_y
         self.move(ctx, dx, dy)
         self.drag_start_x = event.x
         self.drag_start_y = event.y
-    
+
     def drag_pet_release(self, event):
         print(self.speak())
 
-class InitialPositionType(Enum):
-    SET = "SET"
-    RANDOMIZED = "RANDOMIZED"
 
-class MovementType(Enum):
-    WRAP = "WRAP"
-    BOUNCE = "BOUNCE"
+def spawn_pet(
+    ctx,
+    initial_position=[0, 0],
+    pet_config: PetConfig = None,
+):
+    pos = [0, 0]
+    dir = [0, 0]
 
-class PetConfigOptions(Enum):
-    INITIAL_POSITION_TYPE = InitialPositionType
-    MOVEMENT_TYPE = MovementType
+    match pet_config.initial_position_type[0].value:
+        case InitialPositionType.SET.value:
+            pos = [initial_position[0], initial_position[1]]
 
-class PetConfig:
-    def __init__(self, name, move_speed, image_path, dialogue):
-        self.name = name
-        self.move_speed = move_speed
-        self.image_path = image_path
-        self.dialogue = dialogue
+        case InitialPositionType.RANDOMIZED.value:
+            pos[0] = random.randint(0, int(ctx.displays[0].width))
+            pos[1] = random.randint(0, int(ctx.displays[0].height))
 
-def spawn_pet(ctx, image_path, name="empty", position=[0,0], move_speed=10):
-    posX = random.randint(0, int(ctx.displays[0].width))
-    posY = random.randint(0, int(ctx.displays[0].height))
-    dirX = int(random.choice([-3,-2,-1,1,2,3])) # a little silly
-    dirY = int(random.choice([-3,-2,-1,1,2,3]))
+    match pet_config.direction_type[0].value:
+        case DirectionType.RANDOMIZED_ONCE.value:
+            dir[0] = int(random.choice([-3, -2, -1, 1, 2, 3]))  # a little silly
+            dir[1] = int(random.choice([-3, -2, -1, 1, 2, 3]))
 
-    tk_img = py_assets.load_image(image_path, True)
-    shape = ctx.canvas.create_image(0, 0, image=tk_img, anchor=tk.CENTER) # pivot point in center of image
-    pet = Pet(name=name, position=[posX, posY], move_speed=move_speed, current_direction=[dirX, dirY], shape=shape, tk_img=tk_img)
+    tk_img = py_assets.load_image(pet_config.image_path, True)
+    shape = ctx.canvas.create_image(
+        0, 0, image=tk_img, anchor=tk.CENTER
+    )  # pivot point in center of image
+    pet = Pet(
+        name=pet_config.name,
+        position=pos,
+        move_speed=pet_config.move_speed,
+        velocity=dir,
+        shape=shape,
+        tk_img=tk_img,
+        pet_config=pet_config,
+    )
     pet.set_pos(ctx, pet.position[0], pet.position[1])
-    
+
     ctx.canvas.tag_bind(pet.shape, "<ButtonPress-1>", pet.drag_pet_start)
     ctx.canvas.tag_bind(pet.shape, "<B1-Motion>", lambda e: pet.drag_pet_move(e, ctx))
     ctx.canvas.tag_bind(pet.shape, "<ButtonRelease-1>", pet.drag_pet_release)
 
     ctx.pets.append(pet)
 
+
 def update_pets(ctx):
     for pet in ctx.pets:
-        pet.move(ctx, pet.current_direction[0], pet.current_direction[1])
+        pet.move(ctx, pet.velocity[0], pet.velocity[1])
 
-        # Wrap around screen edges
-        if(pet.position[0] > ctx.displays[0].width):
-            pet.set_pos(ctx, 0, pet.position[1])
-        elif(pet.position[0] < 0):
-            pet.set_pos(ctx, ctx.displays[0].width, pet.position[1])
-        
-        if(pet.position[1] > ctx.displays[0].height):
-            pet.set_pos(ctx, pet.position[0], 0)
-        elif(pet.position[1] < 0):
-            pet.set_pos(ctx, pet.position[0], ctx.displays[0].height)
-    
+        # temporary note: 32 is "border" pixel amount to prevent pets from going off screen and killing python
+
+        match pet.pet_config.border_reaction_type[0].value:
+            case (
+                BorderReactionType.BOUNCE.value
+            ):  # Switch directions on contact with border
+                if (
+                    pet.position[0] > (ctx.displays[0].width - 16)
+                    or pet.position[0] < 16
+                ):
+                    pet.velocity = [
+                        -pet.velocity[0],
+                        pet.velocity[1],
+                    ]
+
+                if (
+                    pet.position[1] > (ctx.displays[0].height - 16)
+                    or pet.position[1] < 16
+                ):
+                    pet.velocity = [
+                        pet.velocity[0],
+                        -pet.velocity[1],
+                    ]
+
+            case BorderReactionType.WRAP.value:  # Wrap around border to opposite end
+                if pet.position[0] > ctx.displays[0].width:
+                    pet.set_pos(ctx, 0, pet.position[1])
+                elif pet.position[0] < 0:
+                    pet.set_pos(ctx, ctx.displays[0].width, pet.position[1])
+
+                if pet.position[1] > ctx.displays[0].height:
+                    pet.set_pos(ctx, pet.position[0], 0)
+                elif pet.position[1] < 0:
+                    pet.set_pos(ctx, pet.position[0], ctx.displays[0].height)
+                pass
+
     ctx.root.after(20, lambda: update_pets(ctx))
-#endregion
+
+
+# endregion
